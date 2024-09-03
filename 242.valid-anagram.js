ff240329c13d73d6f094dfb9766700aab525d8d6
jaydenteo <jyteo@ampol.com.au>
2024-09-03 11:48:08 +0800

    Create 242.valid-anagram.js

---
+++ 242.valid-anagram.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=242 lang=javascript
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) return false;
+
+  const first = s.split("").sort().join("");
+  const second = t.split("").sort().join("");
+
+  return first === second;
+};
+// @lc code=end
+
