2837463f061de4729abd40ce292b6e54aa71062e
jaydenteo <jayden228@gmail.com>
2024-09-05 10:01:25 +0800

    Create 20.valid-parentheses.js

---
+++ Stack/20.valid-parentheses.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=20 lang=javascript
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  const map = new Map([
+    ['}', '{'],
+    [')', '('],
+    [']', '['],
+  ]);
+
+  const stack = [];
+
+  for (const c of s) {
+      if (map.has(c)) {
+          if (map.get(c) !== stack.pop()) {
+              return false;
+          }
+      } else {
+          stack.push(c)
+      }
+  }
+
+  return !stack.length;
+};
+// @lc code=end
+
+// Time O(N) to iterate through the string
+// Space O(N) to add to the stack (could be all open brackets at the worse case)
