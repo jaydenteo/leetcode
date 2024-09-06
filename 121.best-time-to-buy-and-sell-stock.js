6201355d0ab55f5f36c491c7441cd8852baa18ce
jaydenteo <jayden228@gmail.com>
2024-09-06 11:24:26 +0800

    Create 121.best-time-to-buy-and-sell-stock.js

---
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=121 lang=javascript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let [buy, sell, maxProfit] = [0, 1, 0];
+
+  while (sell < prices.length) {
+    if (prices[buy] < prices[sell]) {
+      maxProfit = Math.max(maxProfit, prices[sell] - prices[buy]);
+    } else {
+      buy = sell;
+    }
+
+    sell += 1;
+  }
+
+  return maxProfit;
+};
+// @lc code=end
+
+// Time O(n) to traverse prices array
+// Space O(1) to store pointers and maxProfit
