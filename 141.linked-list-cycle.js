cacd41e65a4978955229a67340ae753e1c77cab9
jaydenteo <jayden228@gmail.com>
2024-09-08 20:57:22 +0800

    Create 141.linked-list-cycle.js

---
+++ 141.linked-list-cycle.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=141 lang=javascript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  const visited = new Set();
+
+  while (head) {
+    if (visited.has(head)) {
+      return true;
+    }
+    visited.add(head);
+    head = head.next;
+  }
+
+  return false;
+};
+// @lc code=end
+
+// Time O(n) to traverse list
+// Space O(n) to add to hashset
+
+// Time O(n) to traverse list
+// Space O(1) to store pointers
+var hasCycle = function (head) {
+  let [fast, slow] = [head, head];
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
