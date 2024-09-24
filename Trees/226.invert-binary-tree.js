5072afce97a6cfd75071e3467ae3c88715686114
jaydenteo <jayden228@gmail.com>
2024-09-24 11:03:58 +0800

    Update 226.invert-binary-tree.js

--- Trees/226.invert-binary-tree.js
+++ Trees/226.invert-binary-tree.js
@@ -14,11 +14,26 @@
  * }
  */
 /**
+ * Time O(n) to visit each node once
+ * Space O(logn) if a balanced binary tree
+ * Space O(n) if a skewed binary tree
+ *
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function(root) {
-    
+var invertTree = function (root) {
+  // Base case: If the node is null, there's nothing to invert
+  if (root === null) {
+    return null;
+  }
+
+  // Recursive case: Invert the left and right subtrees
+  invertTree(root.left);
+  invertTree(root.right);
+
+  // Combine results: Swap the left and right children
+  [root.left, root.right] = [root.right, root.left];
+
+  return root;
 };
 // @lc code=end
-
