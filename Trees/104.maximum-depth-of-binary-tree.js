d3a1c5e4b142b488d2fe22a3b2a8cf2ff7d667ed
jaydenteo <jayden228@gmail.com>
2024-09-25 14:33:41 +0800

    Update 104.maximum-depth-of-binary-tree.js

--- Trees/104.maximum-depth-of-binary-tree.js
+++ Trees/104.maximum-depth-of-binary-tree.js
@@ -14,11 +14,80 @@
  * }
  */
 /**
+ * Recursive DFS (Inorder)
+ * Time O(n) because it visits every node in the tree once
+ * Space O(h) where h is the height of the tree, due to the recursive call stack
+ *
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
-    
+var maxDepth = function (root) {
+  // Base case
+  if (!root) {
+    return 0;
+  }
+
+  // Recursive function
+  let leftSize = maxDepth(root.left);
+  let rightSize = maxDepth(root.right);
+
+  // Combine results
+  return 1 + Math.max(leftSize, rightSize);
 };
 // @lc code=end
 
+/**
+ * Iterative DFS (Preorder)
+ * Time O(n) because it visits every node in the tree once
+ * Space O(h) due to the stack data structure
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  const stack = [[root, 0]];
+  let maxDepth = 0;
+
+  while (stack.length) {
+    let [node, depth] = stack.pop();
+
+    maxDepth = Math.max(maxDepth, depth + 1);
+
+    if (node.right) stack.push([node.right, depth + 1]);
+    if (node.left) stack.push([node.left, depth + 1]);
+  }
+
+  return maxDepth;
+};
+
+/**
+ * Iterative BFS
+ * Time O(n^2) because it visits each node and shifting the queue takes O(n) time
+ * Space O(w) where w is the maximum width of the tree
+ *
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  const queue = [[root, 0]];
+  let maxDepth = 0;
+
+  while (queue.length) {
+    let [node, depth] = queue.shift();
+
+    maxDepth = Math.max(maxDepth, depth + 1);
+
+    if (node.right) queue.push([node.right, depth + 1]);
+    if (node.left) queue.push([node.left, depth + 1]);
+  }
+
+  return maxDepth;
+};
