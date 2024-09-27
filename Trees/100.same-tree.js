c90ae99afd04712e97d2f939f322b51b9a19eb79
jaydenteo <jayden228@gmail.com>
2024-09-27 14:38:03 +0800

    Complete 572 Subtree of another Tree

--- Trees/100.same-tree.js
+++ Trees/100.same-tree.js
@@ -14,12 +14,25 @@
  * }
  */
 /**
+ * Time O(p + q) to visit each node once
+ * Space O(h) due to the height of the trees
+ *
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-    
+var isSameTree = function (p, q) {
+  // Base cases
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+
+  // Recursive functions
+  const leftEqual = isSameTree(p.left, q.left);
+  const rightEqual = isSameTree(p.right, q.right);
+
+  // Combine results
+  return leftEqual && rightEqual;
 };
 // @lc code=end
 
+export default isSameTree;
--- Trees/572.subtree-of-another-tree.js
+++ Trees/572.subtree-of-another-tree.js
@@ -4,6 +4,8 @@
  * [572] Subtree of Another Tree
  */
 
+import isSameTree from "./100.same-tree";
+
 // @lc code=start
 /**
  * Definition for a binary tree node.
@@ -14,12 +16,26 @@
  * }
  */
 /**
+ * Time O(m * n), where m is the number of nodes in the main tree (root) and n is the
+ * number of nodes in the subtree (subRoot), because for each node in the main tree,
+ * we may need to check all nodes in the subtree.
+ * Space O(h) due to the recursion call stack
+ *
  * @param {TreeNode} root
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
-var isSubtree = function(root, subRoot) {
-    
+var isSubtree = function (root, subRoot) {
+  // Base cases
+  if (!subRoot) return true;
+  if (!root) return false; // if root is null and subRoot is not, it can't be a subtree
+
+  // Recursive functions
+  if (isSameTree(root, subRoot)) return true;
+  const leftMatches = isSubtree(root.left, subRoot);
+  const rightMatches = isSubtree(root.right, subRoot);
+
+  // Combine results
+  return leftMatches || rightMatches;
 };
 // @lc code=end
-
