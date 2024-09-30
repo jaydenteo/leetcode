33354a8e37d7ef17040feedea606333b35a42d39
jaydenteo <jayden228@gmail.com>
2024-09-30 22:59:42 +0800

    Update 347.top-k-frequent-elements.js

--- Arrays and Hashing/347.top-k-frequent-elements.js
+++ Arrays and Hashing/347.top-k-frequent-elements.js
@@ -6,12 +6,24 @@
 
 // @lc code=start
 /**
+ * Hashmap Sorting
+ * Time O(nlogn) as sorting takes nlogn time
+ * Space O(n) for the hashmap and array
+ *
+ *
  * @param {number[]} nums
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
-    
+var topKFrequent = function (nums, k) {
+  const freqMap = new Map();
+
+  for (let num of nums) {
+    freqMap.set(num, (freqMap.get(num) || 0) + 1);
+  }
+
+  return [...freqMap.keys()]
+    .sort((a, b) => freqMap.get(b) - freqMap.get(a))
+    .slice(0, k);
 };
 // @lc code=end
-
