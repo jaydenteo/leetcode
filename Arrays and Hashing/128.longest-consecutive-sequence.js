ab939c0a0d19fa6d7981b9f7f42d5a8cd7bb1899
jaydenteo <jayden228@gmail.com>
2024-10-03 18:49:06 +0800

    Update 128.longest-consecutive-sequence.js

--- Arrays and Hashing/128.longest-consecutive-sequence.js
+++ Arrays and Hashing/128.longest-consecutive-sequence.js
@@ -6,11 +6,33 @@
 
 // @lc code=start
 /**
+ * Time O(n) to traverse the hashset
+ * Space O(n) for the hashset
+ *
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
-    
+var longestConsecutive = function (nums) {
+  const numSet = new Set(nums);
+  let maxLength = 0;
+
+  for (let num of numSet) {
+    // Start a new sequence only if the current number is the start of the sequence
+    if (!numSet.has(num - 1)) {
+      let currentLength = 1;
+      let nextNum = num + 1;
+
+      // Count the length of the sequence
+      while (numSet.has(nextNum)) {
+        currentLength++;
+        nextNum++;
+      }
+
+      // Update the maximum length if the current sequence is longer
+      maxLength = Math.max(maxLength, currentLength);
+    }
+  }
+
+  return maxLength;
 };
 // @lc code=end
-
