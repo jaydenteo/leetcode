40a6268c82153d6e203f4d0b67973a323025a68a
jaydenteo <jayden228@gmail.com>
2024-10-07 17:57:33 +0800

    Update 167.two-sum-ii-input-array-is-sorted.js

--- Two Pointers/167.two-sum-ii-input-array-is-sorted.js
+++ Two Pointers/167.two-sum-ii-input-array-is-sorted.js
@@ -6,12 +6,24 @@
 
 // @lc code=start
 /**
+ * Time O(n) as each iteration increments left or right reducing the problem size by one each time
+ * Space O(1)
+ *
  * @param {number[]} numbers
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
-    
+var twoSum = function (numbers, target) {
+  let [left, right] = [0, numbers.length - 1];
+
+  while (left < right) {
+    const total = numbers[left] + numbers[right];
+
+    if (total > target) right--;
+    else if (total < target) left++;
+    else return [left + 1, right + 1];
+  }
+
+  return -1;
 };
 // @lc code=end
-
