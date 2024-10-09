5ef8de1d48b82865e21ed22797eb90bb341959eb
jaydenteo <jayden228@gmail.com>
2024-10-09 10:59:03 +0800

    Update 11.container-with-most-water.js

--- Two Pointers/11.container-with-most-water.js
+++ Two Pointers/11.container-with-most-water.js
@@ -6,11 +6,24 @@
 
 // @lc code=start
 /**
+ * Time O(n) to traverse height array
+ * Space O(1) to store pointers and maxArea result
+ *
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    
+var maxArea = function (height) {
+  let [l, r] = [0, height.length - 1];
+  let maxArea = 0;
+
+  while (l < r) {
+    const area = (r - l) * Math.min(height[l], height[r]);
+    maxArea = Math.max(maxArea, area);
+
+    if (height[l] > height[r]) r--;
+    else l++;
+  }
+
+  return maxArea;
 };
 // @lc code=end
-
