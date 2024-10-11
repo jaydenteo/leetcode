9dab60d5a5e30d4fd2d2125914f4c9ce41491e2e
jaydenteo <jayden228@gmail.com>
2024-10-11 14:01:16 +0800

    Update 150.evaluate-reverse-polish-notation.js

--- Stack/150.evaluate-reverse-polish-notation.js
+++ Stack/150.evaluate-reverse-polish-notation.js
@@ -6,11 +6,32 @@
 
 // @lc code=start
 /**
+ * Time O(n) to traverse the array
+ * Space O(n) to store tokens in the stack
+ *
  * @param {string[]} tokens
  * @return {number}
  */
-var evalRPN = function(tokens) {
-    
+var evalRPN = function (tokens) {
+  const stack = [];
+
+  const operations = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => Math.trunc(a / b),
+  };
+
+  for (const token of tokens) {
+    if (operations[token]) {
+      const b = stack.pop();
+      const a = stack.pop();
+      stack.push(operations[token](a, b));
+    } else {
+      stack.push(Number(token));
+    }
+  }
+
+  return stack.pop();
 };
 // @lc code=end
-
