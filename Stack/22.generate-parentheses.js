f8831419c313905f37c4a0b19674635cac1633fd
jaydenteo <jayden228@gmail.com>
2024-10-15 18:05:30 +0800

    Update 22.generate-parentheses.js

--- Stack/22.generate-parentheses.js
+++ Stack/22.generate-parentheses.js
@@ -6,11 +6,31 @@
 
 // @lc code=start
 /**
+ * DFS Recursive Backtracking
+ *
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function(n) {
-    
+var generateParenthesis = function (n) {
+  const result = [];
+
+  const dfs = (currentString, open, close) => {
+    if (currentString.length === n * 2) {
+      result.push(currentString);
+      return;
+    }
+
+    if (open < n) {
+      dfs(currentString + "(", open + 1, close);
+    }
+
+    if (close < open) {
+      dfs(currentString + ")", open, close + 1);
+    }
+  };
+
+  dfs("", 0, 0);
+
+  return result;
 };
 // @lc code=end
-
