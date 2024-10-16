17e8d636da9b41b4ad298df84c4b11e641db51c1
jaydenteo <jayden228@gmail.com>
2024-10-16 10:26:42 +0800

    Update 739.daily-temperatures.js

--- Stack/739.daily-temperatures.js
+++ Stack/739.daily-temperatures.js
@@ -6,11 +6,51 @@
 
 // @lc code=start
 /**
+ * Monotonic Decreasing Stack
+ * Time O(n) to traverse the temperatures
+ * Space O(n) to store in the stack
+ *
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-    
+var dailyTemperatures = function (temperatures) {
+  const stack = [];
+  const result = new Array(temperatures.length).fill(0);
+
+  for (let i = 0; i < temperatures.length; i++) {
+    while (stack.length && stack[stack.length - 1][1] < temperatures[i]) {
+      const [index, value] = stack.pop();
+      result[index] = i - index;
+    }
+
+    stack.push([i, temperatures[i]]); // I could have just stored the indexes and compared by doing temperatures[stack[stack.length - 1]]
+  }
+
+  return result;
 };
 // @lc code=end
 
+/**
+ * Brute Force [FAILS BY TIME LIMIT EXCEEDED]
+ * Constraint is up to 10^5 inputs so, Time O(n) will result in 10^10 which is too long.
+ * We should aim for 10^8 or less in general
+ *
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+  const result = [];
+
+  for (let i = 0; i < temperatures.length; i++) {
+    for (let j = i + 1; j < temperatures.length; j++) {
+      if (temperatures[j] > temperatures[i]) {
+        result.push(j - i);
+        break;
+      }
+    }
+
+    if (result.length < i + 1) result.push(0);
+  }
+
+  return result;
+};
